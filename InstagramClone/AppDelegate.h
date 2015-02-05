94d8c10d71cbb3e14312eeec98f6a2a40922f9e1
Sarp Centel <sarp@fb.com>
2015-02-05 10:29:53 -0800

    Initial Commit

---
+++ InstagramClone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramClone
+//
+//  Created by Sarp Centel on 2/5/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
